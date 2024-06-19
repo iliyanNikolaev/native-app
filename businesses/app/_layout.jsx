d880a79c4f8ac414713927a097e35f95079f9ed7
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-06-19 08:43:11 +0300

    caching auth session

--- businesses/app/_layout.jsx
+++ businesses/app/_layout.jsx
@@ -2,11 +2,30 @@ import { Stack } from "expo-router";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { Text } from "react-native";
 import LoginScreen from "@/components/LoginScreen";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function RootLayout() {
   return (
     <ClerkProvider
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
     >
       <SignedIn>
         <Stack screenOptions={{ headerShown: false }}>
